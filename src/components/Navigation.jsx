e58cefada69e49f1b2aa823c914547d7fe88ee8d
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-22 18:36:35 +0100

    refactor: Move sign-up logic to the editing website

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
 export default function Navigation() {
-  const [userId, setUserId] = useState("");
   const [userToken, setUserToken] = useState("");
 
   function logoutUser() {
@@ -11,36 +10,18 @@ export default function Navigation() {
 
   useEffect(() => {
     const userToken = sessionStorage.getItem("apiPoweredBlogToken");
-    const userDataJson = sessionStorage.getItem("apiPoweredBlogUserData");
-    const userData = userDataJson && JSON.parse(userDataJson);
     userToken && setUserToken(userToken);
-    userData?.id && setUserId(userData.id);
   }, []);
 
   return (
     <div className="nav-links">
-      <a href="/" className="nav-link">
-        Home
-      </a>
-      {!userId && (
-        <a href="/sign-up" className="nav-link">
-          Sign up
-        </a>
-      )}
-      {!userToken && (
-        <a href="/log-in" className="nav-link">
-          Log in
-        </a>
-      )}
+      <a href="/">Home</a>
+      {!userToken && <a href="/log-in">Staff Log in</a>}
       {userToken && (
-        <a href="http://localhost:4322" className="nav-link">
-          Manage Posts
-        </a>
-      )}
-      {userToken && (
-        <span className="nav-link" onClick={logoutUser}>
-          Log out
-        </span>
+        <>
+          <a href="http://localhost:4322">Manage Posts</a>
+          <span onClick={logoutUser}>Log out</span>
+        </>
       )}
     </div>
   );
