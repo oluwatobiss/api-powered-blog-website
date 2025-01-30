adcc3661b6bb735f6cb6e70b008f9a6d15621432
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-01-30 13:17:06 +0100

    docs: Move log-in logic from the consumption website to the editing

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,25 +1,7 @@
-import { useEffect, useState } from "react";
-
 export default function Navigation() {
-  const [userToken, setUserToken] = useState("");
-
-  useEffect(() => {
-    let userToken = localStorage.getItem("apiPoweredBlogToken");
-
-    console.log("=== Website's Navigation useEffect ===");
-    console.log(userToken);
-
-    userToken === "undefined" && (userToken = undefined);
-    userToken && setUserToken(userToken);
-  }, []);
-
   return (
     <div className="nav-links">
-      <a href="/">Home</a>
-      {!userToken && <a href="/log-in">Staff Log in</a>}
-      {userToken && (
-        <a href={import.meta.env.PUBLIC_STAFFEND_URI}>Manage Posts</a>
-      )}
+      <a href="/">fansInSync</a>
     </div>
   );
 }
---
+++ src/components/UserConfigLogic.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+
+export default function UserConfigLogic() {
+  useEffect(() => {
+    const expectedOrigin = import.meta.env.PUBLIC_STAFFEND_URI;
+    function handleMessageEvent(e) {
+      console.log("=== UserConfigLogic ===");
+      console.log(e);
+
+      if (e.origin === expectedOrigin) {
+        const userData = e.data;
+        if (userData !== null) {
+          localStorage.setItem("apiPoweredBlogToken", userData.token);
+          localStorage.setItem(
+            "apiPoweredBlogUserData",
+            JSON.stringify(userData.payload)
+          );
+        }
+      }
+    }
+    window.addEventListener("message", handleMessageEvent);
+    return () => window.removeEventListener("message", handleMessageEvent);
+  });
+  return (
+    <p>
+      This page is solely for adding the user's token and data to the web
+      storage.
+    </p>
+  );
+}
