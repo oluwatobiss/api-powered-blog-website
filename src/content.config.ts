ff365fb4b2ba59aaf8e8ccaf046929a6f20f7466
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-02-02 22:23:34 +0100

    refactor: Move import.meta code into a variable

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,10 @@
 import { defineCollection, z } from "astro:content";
 
+const backendUri = import.meta.env.PUBLIC_BACKEND_URI;
+
 const blog = defineCollection({
   loader: async () => {
-    const response = await fetch(`${import.meta.env.PUBLIC_BACKEND_URI}/posts`);
+    const response = await fetch(`${backendUri}/posts`);
     const data = await response.json();
     return data.map((post: any) => ({
       id: `${post.id}`,
